517bb5cbb4beb6139c3849a96929f82183744a7f
Thalles Carneiro <thallescarneiro@gmail.com>
2022-02-16 23:50:35 -0300

    feat: adding cors use in app file

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 const { connection } = require('./models/db');
 const taskRouter = require('./routers/taskRouter');
@@ -8,6 +9,7 @@ connection();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
