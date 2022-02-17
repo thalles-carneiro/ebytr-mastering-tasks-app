818d3467214a52b662f6767dfe0d8b30427a5252
Thalles Carneiro <thallescarneiro@gmail.com>
2022-02-16 21:39:39 -0300

    test: fix connection mock

--- backend/tests/mocks/connection.js
+++ backend/tests/mocks/connection.js
@@ -1,18 +1,13 @@
 const mongoose = require('mongoose');
 const { MongoMemoryServer } = require('mongodb-memory-server');
 
-const DB_NAME = 'MasteringTasksApp';
-const OPTIONS = {
-  dbName: DB_NAME,
-};
-
 const getConnection = async () => {
   try {
     const DB_SERVER = await MongoMemoryServer.create();
     const DB_URI = DB_SERVER.getUri();
 
-    const connection = mongoose.createConnection(DB_URI, OPTIONS);
-    return connection;
+    const conn = await mongoose.connect(DB_URI);
+    return conn.connection;
   } catch (error) {
     console.error(error);
     process.exit(1);
