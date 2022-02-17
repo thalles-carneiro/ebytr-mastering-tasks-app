6c9cca9bda5281b92834eb82bee26e066613dcb2
Thalles Carneiro <thallescarneiro@gmail.com>
2022-02-16 22:17:36 -0300

    feat: adding task service functions

---
+++ backend/src/services/tasks/index.js
@@ -0,0 +1,16 @@
+const TaskModel = require('../../models/tasks');
+
+const getAll = async () => TaskModel.getAll();
+
+const add = async (task) => TaskModel.add({ task });
+
+const update = async (id, task) => TaskModel.update(id, { task });
+
+const remove = async (id) => TaskModel.delete(id);
+
+module.exports = {
+  getAll,
+  update,
+  delete: remove,
+  add,
+};
